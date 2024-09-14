bf79380afebccfc84731e8aed80c037b62d84a80
domingouribe <uribe.domingo@gmail.com>
2024-09-14 12:30:23 -0300

    Image on Brand component fixed

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,10 @@
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 const siteInfo = {
   name: "Muller And Kong Travel",
   description: "Tour en la Región de Coquimbo",
-  logoUrl: "/src/assets/logo.png",
+  // logoUrl: "./src/assets/logo.png",
+  logoUrl: "./src/assets/logo.png",
   contact: {
     email: "hola@mullerandkong.com",
     telephone: "+56 9 7389 7560",
@@ -19,22 +20,21 @@ const menu = [
   {
     title: "Inicio",
     url: "/",
-    id: uuidv4(),
+    // id: uuidv4(),
   },
   {
-    title: "Tours",
-    url: "/tours",
-    id: uuidv4(),
+    title: "Tours", url: "/tours",
+    // id: uuidv4(),
   },
   {
     title: "Blog",
     url: "/blog",
-    id: uuidv4(),
+    // id: uuidv4(),
   },
   {
     title: "Contacto",
     url: "/contact",
-    id: uuidv4(),
+    // id: uuidv4(),
   },
 ];
 
